a4e8735319bf295b5ba7402e206a8155f9f9f5d0
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-10 23:32:32 +0100

    First commit

---
+++ bin/bats_and_cats.dart
@@ -0,0 +1,5 @@
+import 'package:bats_and_cats/bats_and_cats.dart' as bats_and_cats;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bats_and_cats.calculate()}!');
+}
---
+++ test/bats_and_cats_test.dart
@@ -0,0 +1,8 @@
+import 'package:bats_and_cats/bats_and_cats.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

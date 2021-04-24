4db8ebeffdb686cae89f2a2158f31d8c7e35c667
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-24 11:25:35 +0100

    Turn into webapp

--- bin/bats_and_cats.dart
+++
@@ -1,5 +0,0 @@
-import 'package:bats_and_cats/bats_and_cats.dart' as bats_and_cats;
-
-void main(List<String> arguments) {
-  print('Hello world: ${bats_and_cats.calculate()}!');
-}
--- test/bats_and_cats_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bats_and_cats/bats_and_cats.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}

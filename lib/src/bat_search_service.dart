f150d523427f836a359c700dd092902867afe1ac
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-07-22 22:57:33 +0000

    Fix typo

--- lib/src/bat_search_service.dart
+++ lib/src/bat_search_service.dart
@@ -7,22 +7,22 @@ import 'bat.dart';
 
 class BatSearchService {
   final Client _http;
-  
+
   BatSearchService(this._http);
-  
+
   Future<List<Bat>> Search(String term) async {
     try {
       final response = await _http.get('app/bats/?name=$term');
       return (_extractData(response) as List)
-        .map((json) => Bat.fromJson(json))
-        .toList;
+          .map((json) => Bat.fromJson(json))
+          .toList();
     } catch (e) {
       throw _handleError(e);
     }
   }
-  
-  dynamic _extractData(Response resp) =>json.decode(resp.body)['data'];
-  
+
+  dynamic _extractData(Response resp) => json.decode(resp.body)['data'];
+
   Exception _handleError(dynamic e) {
     print(e); // for demo puroses only
     return Exception('Server error; cause: $e');
